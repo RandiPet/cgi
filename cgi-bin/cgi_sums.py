864a1a10af97fb9336a3d2a7e5b64d1143576775
RandiPet <47512663+RandiPet@users.noreply.github.com>
2019-11-30 19:22:15 -0800

    CGI activity completed

--- cgi-bin/cgi_sums.py
+++ cgi-bin/cgi_sums.py
@@ -4,6 +4,16 @@ import cgitb
 
 cgitb.enable()
 
+form = cgi.FieldStorage()
+operands = form.getlist('operand')
+
+try:
+    total = sum(map(int, operands))
+    body = "Your total is: {}".format(total)
+
+except (ValueError, TypeError):
+    body = "Calculation failed. Integers only please!"
+
 print("Content-type: text/plain")
 print()
-print("Your job is to make this work")
+print(body)
